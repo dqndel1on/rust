f93717f1af7b9cc2f8d4f030ba91e25806eeb0fe
sanjipun <iamsanjipun@gmail.com>
2022-01-23 14:50:55 +0545

    arrays

---
+++ src/arrays.rs
@@ -0,0 +1,8 @@
+pub fn _run() {
+    let numbers: [i32; 5] = [1, 2, 3, 4, 5]; // parameters first is type and 2nd is array length
+
+    println!("{:?}", numbers);
+    println!("{:?}", numbers[0]);
+
+    //other methods, length, memory stack allocated, slice, replace etx
+}
