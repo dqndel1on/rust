71f67912a5a2d7ef2cff025517d42b0b880c7df7
sanjipun <iamsanjipun@gmail.com>
2022-01-23 14:51:08 +0545

    enums

---
+++ src/enums.rs
@@ -0,0 +1,27 @@
+enum Movement {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+fn move_avatar(m: Movement) {
+    // Perform action depending on info
+    match m {
+        Movement::Up => println!("Avatar moving Upp"),
+        Movement::Down => println!("Avatar moving Down"),
+        Movement::Left => println!("Avatar moving Left"),
+        Movement::Right => println!("Avatar moving Right"),
+    }
+}
+pub fn _run() {
+    let avatar1 = Movement::Left;
+    let avatar2 = Movement::Right;
+    let avatar3 = Movement::Up;
+    let avatar4 = Movement::Down;
+
+    move_avatar(avatar1);
+    move_avatar(avatar2);
+    move_avatar(avatar3);
+    move_avatar(avatar4);
+}
