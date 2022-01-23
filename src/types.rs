a0b2128932d375504869c4e388f8bcc5d5069f7d
sanjipun <iamsanjipun@gmail.com>
2022-01-23 14:52:00 +0545

    types

---
+++ src/types.rs
@@ -0,0 +1,13 @@
+pub fn _run() {
+    let a: i32 = 1;
+
+    let b: i64 = 123123123123123123;
+
+    //find max size
+    println!("Max i32: {}", std::i32::MAX);
+    //boolean
+
+    //char -- in single quote and should be single character.
+    let a1 = 'a';
+    println!("{:?}", (a, b, a1))
+}
