dddb5ecdb9690ec9b90ad8a19eedc65938ffee70
sanjipun <iamsanjipun@gmail.com>
2022-01-23 14:51:37 +0545

    print

---
+++ src/print.rs
@@ -0,0 +1,10 @@
+pub fn _run() {
+    //Print to console
+    println!("Hello print.rs");
+
+    //Basic Formatting
+    println!("{} is from {}", "Sanji", "KTM");
+
+    //Positional ARguments
+    println!("{0} is from {1} and {0} is {2}", "Sanji", "KTM", "Cool");
+}
