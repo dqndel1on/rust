e043b6ff22ef00552aa3acac6643c26e56c8ba3f
sanjipun <iamsanjipun@gmail.com>
2022-01-23 14:51:26 +0545

    main file

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+#![allow(non_snake_case)]
+//mod arrays;
+//mod conditionals;
+//mod loops;
+//mod print;
+//mod strings;
+//mod tuples;
+//mod types;
+//mod vars;
+//mod vectors;
+//mod functions;
+//mod pointer_ref;
+//mod structs;
+mod enums;
+
+fn main() {
+    //print::_run();
+    //vars::_run();
+    //types::_run();
+    //strings::_run();
+    //tuples::_run();
+    //arrays::_run();
+    //vectors::_run();
+    //conditionals::_run();
+    //loops::_run();
+    //functions::_run();
+    //pointer_ref::_run();
+    //structs::_run();
+    enums::_run();
+}
