591d40722f9579bce8ab9884667062a2d253b74a
sanjipun <iamsanjipun@gmail.com>
2022-01-23 14:51:02 +0545

    conditions

---
+++ src/conditionals.rs
@@ -0,0 +1,9 @@
+pub fn _run() {
+    let age = 16;
+
+    if age >= 20 {
+        println!("You aren't teen anymore!");
+    } else {
+        println!("You might be teen! Hmm... or a toddler!");
+    }
+}
